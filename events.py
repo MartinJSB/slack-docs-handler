706aa634b8ad922831503994ff88fa2566c91a5b
MartinJSB <martin.brolin@futurice.com>
2025-04-11 19:24:18 +0200

    remove video to audio conversion, audio compression and file system storage

--- events.py
+++ events.py
@@ -1,9 +1,9 @@
 from slack_bolt import App
 from slack_bolt.context.respond import Respond
 
-from tools.compress_audio import compress_wav
-from tools.drive_video_to_audio import download_audio_from_drive
-from tools.speech_to_text import transcribe_audio
+# from tools.compress_audio import compress_wav
+# from tools.drive_video_to_audio import download_audio_from_drive
+# from tools.speech_to_text import transcribe_audio
 
 def register_events(slack_app: App):
     @slack_app.event("message")
@@ -31,15 +31,17 @@ def register_events(slack_app: App):
                 respond("The provided link is not a valid Google Drive link.")
                 return
 
-            # Attempt to download the video and extract the audio.
-            audio_result = download_audio_from_drive(drive_link)
-            if audio_result["status"] != "success":
-                respond(f"Error: {audio_result['message']}")
+            # # Attempt to download the video and extract the audio.
+            # audio_result = download_audio_from_drive(drive_link)
+            # if audio_result["status"] != "success":
+            #     respond(f"Error: {audio_result['message']}")
 
-            # Reduce file size if needed
-            compressed_audio = compress_wav(audio_result["file_path"])
+            # # Reduce file size if needed
+            # compressed_audio = compress_wav(audio_result["file_path"])
             
-            transcription_result = transcribe_audio(compressed_audio)
-            respond(transcription_result)
+            # transcription_result = transcribe_audio(compressed_audio)
+            # respond(transcription_result)
+            respond( f"You sent: {drive_link}")
+            return 
         except Exception as e:
             respond(f"An unexpected error occurred: {str(e)}")
